b1ac814f253cce369409e8fb0f89f708cb8d4e78
AlejandroSosaG <93648834+AlejandroSosaG@users.noreply.github.com>
2023-10-27 12:19:38 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
